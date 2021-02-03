dfc60316f7936035afcd758acb59d414b56c92a5
zentaker <apalomino12@icloud.com>
2021-02-02 21:37:55 -0500

    subida de pymes digital

---
+++ Pymesdigital/README.md
@@ -0,0 +1,7 @@
+# WEBpymesdigital
+
+Utilizando como referencia de los templates 
+- genius
+- infinity
+
+<img width="1439" alt="Screenshot 2021-01-25 at 5 13 40 PM" src="https://user-images.githubusercontent.com/46875264/105772866-d87be800-5f30-11eb-9e8d-5fe215a21828.png">
---
+++ Pymesdigital/js/custom.js
@@ -0,0 +1,65 @@
+(function ($) {
+
+    "use strict";
+
+        // PRE LOADER
+        $(window).load(function(){
+          $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+        });
+
+
+        // MENU
+        $('.navbar-collapse a').on('click',function(){
+          $(".navbar-collapse").collapse('hide');
+        });
+
+        $(window).scroll(function() {
+          if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+              } else {
+                $(".navbar-fixed-top").removeClass("top-nav-collapse");
+              }
+        });
+        
+        $("#js-rotating").Morphext({
+          // The [in] animation type. Refer to Animate.css for a list of available animations.
+          animation: "fadeIn",
+          // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+          separator: ",",
+          // The delay between the changing of each phrase in milliseconds.
+          speed: 2000,
+          complete: function () {
+            // Called after the entrance animation is executed.
+          }
+          });
+          
+
+
+        // PARALLAX JS
+        function initParallax() {
+          $('#home').parallax("60%", 100);
+          $('#about').parallax("100%", 80);
+          $('#project').parallax("80%", 60);
+          $('#team').parallax("40%", 40);
+          $('#contact').parallax("20%", 20);
+          }
+        initParallax();
+
+
+        // Owl Carousel
+        var owl = $("#owl-team");
+          owl.owlCarousel({
+            autoPlay: 6000,
+            items : 4,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3],
+            itemsTablet: [768,2],
+            itemsTabletSmall: false,
+            itemsMobile : [479,1],
+            Speedfast: 200,
+        });
+
+  
+  
+
+})(jQuery);
